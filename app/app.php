9929b241759f2b2ddf4148bac590a50443619b8e
jared beckler and jessica fix <jared.beckler@gmail.com>
2016-02-16 16:36:01 -0800

    Add beginning functionality for posting a list of saved anagrams:WIP

--- app/app.php
+++ app/app.php
@@ -2,7 +2,17 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/AnagramValidator.php";
 
+    use Symfony\Component\Debug\Debug;
+Debug::enable();
+
+    session_start();
+    if(empty($_SESSION['anagram_list'])) {
+    $_SESSION['anagram_list'] = array();
+    }
+
     $app = new Silex\Application();
+
+    $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
@@ -12,10 +22,16 @@
         return $app["twig"]->render("home.html.twig");
     });
 
+    $app->post("/anagram_confirmation", function() use ($app) {
+        $anagram = new AnagramValidator($_POST['original_word'], $_POST['anagram_compare']);
+        $anagram->save();
+        return $app['twig']->render('confirmation.html.twig', array('newanagram' => $anagram));
+    });
+
     $app->get("/view_results", function() use ($app) {
         $anagramChecker = new AnagramValidator;
         $anagramResult = $anagramChecker->checkAnagram($_GET["original_word"], $_GET["anagram_compare"]);
-        
+
         return $app["twig"]->render("anagram_compare.html.twig", array('results'=>$anagramResult, "displayOriginalWord"=>$_GET["original_word"], "displayAnagramCompare"=>$_GET["anagram_compare"]));
     });
 
--- src/AnagramValidator.php
+++ src/AnagramValidator.php
@@ -1,6 +1,34 @@
 <?php
     class AnagramValidator
     {
+        private $word;
+        private $anagram_test;
+
+
+        function __contruct($inputted_word, $inputted_anagram_test)
+        {
+            $this->$word = $inputted_word;
+            $this->$anagram_test = $inputted_anagram_test;
+        }
+
+        function setWord($inputted_word)
+        {
+            $this->$word = $inputted_word;
+        }
+        function getWord()
+        {
+            return $this->word;
+        }
+
+        function setAnagramTest($inputted_anagram_test)
+        {
+            $this->$anagram_test = $inputted_anagram_test;
+        }
+        function getAnagramTest()
+        {
+            return $this->anagram_test;
+        }
+
         function checkAnagram($input, $comparison)
         {
             $user_input = preg_replace('/[^A-Za-z0-9\-]/', '', $input);
@@ -16,10 +44,22 @@
 
             if($input_array == $comparison_array)
             {
+                array_push($actualAnagrams, $this);
                 return true;
             } else {
+                array_push($notAnAnagram, $this);
                 return false;
             }
+        }
+
+        function save()
+        {
+            array_push($_SESSION['anagram_list'], $this);
+        }
+
+        static function getAll()
+        {
+           return $_SESSION['anagram_list'];
         }
     }
 
